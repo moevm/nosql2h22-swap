7cd833f529645158632a50a69e13716c0c1ed8c4
Савельев Илья <54910571+Indiora@users.noreply.github.com>
2022-11-24 22:44:50 +0300

    update views

--- exchange_site/swap/views.py
+++ exchange_site/swap/views.py
@@ -1,19 +1,25 @@
-from django.http import HttpResponse
-import pymongo
+# from django.http import HttpResponse
+# import pymongo
+from django.views.generic import TemplateView
+# client = pymongo.MongoClient('mongo', 27017, username='admin', password='admin')
 
-client = pymongo.MongoClient('mongo', 27017, username='admin', password='admin')
+
+# def index(request):
+#     dbname = client['swap_db']
+
+#     collection = dbname['swap_collection']
+
+#     message = {
+#         "message": "Hello from db"
+#     }
+
+#     collection.insert_one(message)
+
+#     return HttpResponse(str(collection.find()[0]))
+
+class HomeView(TemplateView):
+    template_name = "home.html"
 
 
-def index(request):
-    dbname = client['swap_db']
-
-    collection = dbname['swap_collection']
-
-    message = {
-        "message": "Hello from db"
-    }
-
-    collection.insert_one(message)
-
-    return HttpResponse(str(collection.find()[0]))
-
+class OfferView(TemplateView):
+    template_name = "offer.html"
