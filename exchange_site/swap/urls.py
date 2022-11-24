1c47823a53abcd207ef7e64cd5168218eb37e1ef
Савельев Илья <54910571+Indiora@users.noreply.github.com>
2022-11-24 22:43:54 +0300

    add urls

--- exchange_site/swap/urls.py
+++ exchange_site/swap/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index
+# from .views import index
+from .views import HomeView, OfferView
 
 
 urlpatterns = [
-    path('', index),
+    # path('', index),
+    path('', HomeView.as_view(), name='home'),
+    path('offer/', OfferView.as_view(), name='offer'),
 ]
