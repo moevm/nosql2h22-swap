c45f32d152024403242291bea2c310c318646c99
Савельев Илья <54910571+Indiora@users.noreply.github.com>
2022-11-30 19:59:02 +0300

    Update urls.py

--- exchange_site/swap/urls.py
+++ exchange_site/swap/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-# from .views import index
-from .views import HomeView, OfferView
+from .views import HomeView, OfferView, AddOfferView, Search, index
 
 
 urlpatterns = [
-    # path('', index),
-    path('', HomeView.as_view(), name='home'),
-    path('offer/', OfferView.as_view(), name='offer'),
+    path('', index),
+    path('home/', HomeView.as_view(), name='home'),
+    path('add_offer/', AddOfferView.as_view(), name='add_offer'),
+    path('search/', Search.as_view(), name='search'),
+    path('offer/<str:slug>/', OfferView.as_view(), name='offer'),
 ]
