cf88e30b73303b9dec770bb059effb7f23212ac6
Савельев Илья <54910571+Indiora@users.noreply.github.com>
2022-12-13 18:12:52 +0300

    add other params

--- exchange_site/swap/forms.py
+++ exchange_site/swap/forms.py
@@ -2,16 +2,32 @@ from django import forms
 
 
 class OfferForm(forms.Form):
+
+    CATEGORY_CHOICES = [
+        ('Мебель', 'Мебель'),
+        ('Одежда', 'Одежда'),
+        ('Бытовая техника', 'Бытовая техника'),
+        ('Посуда', 'Посуда'),
+        ('Продукты', 'Продукты'),
+    ]
+
+    STATE_CHOICES = [
+        ('Новое', 'Новое'),
+        ('Хорошее', 'Хорошее'),
+        ('Среднее', 'Среднее'),
+        ('Требует ремонта', 'Требует ремонта'),
+
+    ]
+
     title = forms.CharField(label="Название", widget=forms.TextInput(attrs={"class": "form-control"}))
     description = forms.CharField(label="Описание", widget=forms.Textarea(attrs={"class": "form-control"}))
-    photo = forms.ImageField(required=False)
-
-    # weight = forms.CharField(label="Вес", widget=forms.TextInput(attrs={"class": "form-control"}))
-    # size = forms.CharField(label="Габариты", widget=forms.TextInput(attrs={"class": "form-control"}))
-    # category = forms.CharField(label="Категория", widget=forms.TextInput(attrs={"class": "form-control"}))
-    # state = forms.CharField(label="Состояние", widget=forms.TextInput(attrs={"class": "form-control"}))
-    # city = forms.CharField(label="Город", widget=forms.TextInput(attrs={"class": "form-control"}))
-    # price = forms.IntegerField(label="Цена", widget=forms.NumberInput(attrs={"class": "form-control"}))
+    photo = forms.ImageField()
+    weight = forms.CharField(label="Вес", widget=forms.TextInput(attrs={"class": "form-control"}))
+    size = forms.CharField(label="Размер", widget=forms.TextInput(attrs={"class": "form-control"}))
+    category = forms.ChoiceField(label="Категория", choices=CATEGORY_CHOICES, widget=forms.Select(attrs={"class": "form-control"}))
+    state = forms.ChoiceField(label="Состояние", choices=STATE_CHOICES, widget=forms.Select(attrs={"class": "form-control"}))
+    city = forms.CharField(label="Город", widget=forms.TextInput(attrs={"class": "form-control"}))
+    price = forms.IntegerField(label="Цена", widget=forms.NumberInput(attrs={"class": "form-control"}))
 
 class ImportOfferFromJSONForm(forms.Form):
     file = forms.FileField(label='Файл')
