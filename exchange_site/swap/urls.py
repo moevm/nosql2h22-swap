35747bd1f5bc10f56c28c1c127f79c474b7390b1
Савельев Илья <54910571+Indiora@users.noreply.github.com>
2022-12-23 22:05:43 +0300

    add edit

--- exchange_site/swap/urls.py
+++ exchange_site/swap/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import HomeView, OfferView, AddOfferView, Search, OfferJSONExportView, ImportOfferFromJSONView, OffersJSONExportView, Login, UserLogout, Register
+from .views import HomeView, OfferView, AddOfferView, Search, OfferJSONExportView, ImportOfferFromJSONView, OffersJSONExportView, Login, UserLogout, Register, UserOffers, EditOfferView
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('add_offer/', AddOfferView.as_view(), name='add_offer'),
+    path('edit_offer/<str:slug>/', EditOfferView.as_view(), name='edit_offer'),
     path('import_offer_from_json/', ImportOfferFromJSONView.as_view(), name='import_offer_from_json'),
     path('search/', Search.as_view(), name='search'),
+    path('my_offers/', UserOffers.as_view(), name='user_offers'),
     path('export_offers_to_json', OffersJSONExportView.as_view(), name='export_offers_to_json'),
     path('offer/<str:slug>/', OfferView.as_view(), name='offer'),
     path('offer/<str:slug>/export', OfferJSONExportView.as_view(), name='export_offer'),
